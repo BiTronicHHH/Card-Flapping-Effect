BiTronicHHH/Card-Flapping-Effect

commit 76b91c838d1a2524e4577b387f23dd224ab36cd3

add warning when using Breadcrumb without its Item (#4435)
* add warning when using Breadcrumb without its Item

fix issue ' Warning: Unknown prop `separator` on <div> tag. When I using Breadcrumb #4403'

ps: 我是个前端beginner， - ( ゜- ゜)つロ， 这是我第一个PR welcome, 如果写错或者还有更好的写法请大神指正。thanks

* remove compatible and use type as condition

按照 @benjycui 的意见去掉了兼容项，并使用type判断

components/breadcrumb/Breadcrumb.tsx
@@ -91,6 +91,10 @@ export default class Breadcrumb extends React.Component<BreadcrumbProps, any> {
       });
     } else if (children) {
       crumbs = React.Children.map(children, (element: any, index) => {
+        warning(
+          element.type === BreadcrumbItem,
+          ' `BreadcrumbItem` is required as the wrapper of chilren element. '
+        );
         return cloneElement(element, {
           separator,
           key: index,

commit 2c77b67b8ebab26eb3f4dbe26c5e2cd4f3a0991b

fix: typo; caused by #4435

components/breadcrumb/Breadcrumb.tsx
@@ -93,7 +93,7 @@ export default class Breadcrumb extends React.Component<BreadcrumbProps, any> {
       crumbs = React.Children.map(children, (element: any, index) => {
         warning(
           element.type === BreadcrumbItem,
-          ' `BreadcrumbItem` is required as the wrapper of chilren element. '
+          ' `BreadcrumbItem` is required as the wrapper of children element. '
         );
         return cloneElement(element, {
           separator,